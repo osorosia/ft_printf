cc2e3616bcbaf8818ad42f8e0e3928dee24ea609
osorosia <64416398+osorosia@users.noreply.github.com>
2022-01-29 23:21:19 +0000

    valgrind

--- .test/test.sh
+++ .test/test.sh
@@ -1,12 +1,13 @@
 
+# printf
+gcc test.c ../libftprintf.a
+./a.out > expect.txt
 
 # ft_printf
 gcc -D FT=1 test.c ../libftprintf.a
 ./a.out > actual.txt
 
-# printf
-gcc test.c ../libftprintf.a
-./a.out > expect.txt
+valgrind --log-file="./valgrind.log" --leak-check=full ./a.out > /dev/null
 
 echo ------start------
 diff --text -U 0 actual.txt expect.txt
