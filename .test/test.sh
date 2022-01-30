a12b27c2b62a191edc295a24d7779067a3019410
osorosia <64416398+osorosia@users.noreply.github.com>
2022-01-30 00:18:34 +0000

    スクリプトを簡略化

--- .test/test.sh
+++ .test/test.sh
@@ -9,19 +9,14 @@ gcc -D FT=1 test.c ../libftprintf.a
 ./a.out > actual.txt
 
 if [ $os == 'Linux' ]; then
-    valgrind --log-file="./leaks.log" --leak-check=full ./a.out > /dev/null
+    valgrind --log-file="./leaks.log" --leak-check=full ./a.out &> /dev/null
 fi
 
 echo ------start------
 diff --text -U 0 actual.txt expect.txt
 
 if [ $os == 'Linux' ]; then
-    check=`cat ./leaks.log | grep 'LEAK SUMMARY' | wc -l`
-    if [ $check -eq 0 ]; then
-        echo LEAKS: OK
-    else
-        echo LEAKS: KO!!!!
-    fi
+    cat ./leaks.log | grep 'ERROR SUMMARY'
 fi
 
 if [ $os == 'Darwin' ]; then
